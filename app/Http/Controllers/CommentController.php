e7fad8fbae6fded3ce77a4f4433d6782d5a63fed
Dato Lomiashvili <lomiashvili.dato5@gmail.com>
2025-03-22 20:43:26 +0400

    add comment store function in backend with tests

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Comment\StoreCommentRequest;
+use App\Models\Comment;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCommentRequest $request, Post $post)
+    {
+        Comment::create([
+            ...$request->validated(),
+            'post_id' => $post->id,
+            'user_id' => $request->user()->id,
+        ]);
+
+        return to_route('posts.show', $post);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Comment $comment)
+    {
+        //
+    }
+}
