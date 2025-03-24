95901cc91e81d2585dcc3951f439ff3fe8b29296
Dato Lomiashvili <lomiashvili.dato5@gmail.com>
2025-03-24 21:40:58 +0400

    add post create page in backend and frontend

---
+++ tests/Feature/Controllers/PostController/CreateTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Models\User;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
+
+it('requires authentication', function () {
+    get(route('posts.create'))->assertRedirectToRoute('login');
+});
+
+it('returns the correct component', function () {
+    actingAs(User::factory()->create())
+        ->get(route('posts.create'))
+        ->assertComponent('Posts/Create')
+    ;
+});
